3fbe0190d3d03273ea0665f0ba966ab691b35e9d
AnnaAleksovna <53649649+AnnaAleksovna@users.noreply.github.com>
2019-11-06 09:41:24 +0200

    Guess the number

---
+++ script.js
@@ -0,0 +1,34 @@
+﻿let r = Math.random();
+r = r * 10;
+r = Math.round(r);
+console.log(r);
+
+let userNum = document.querySelector('#user-num');
+
+document.querySelector('#btn').onclick = checkNum;
+
+function checkNum() {
+	let num = userNum.value;
+	num = parseInt(num);
+	console.log(num);
+		
+	if (isNaN(num)) {
+		alert('Введите корректное число.');
+	}	
+	else {
+		if (num == r) {
+			alert('УРА!!! Вы угадали!');
+			location.reload();
+	}
+	else {
+		if (num > r) {
+		alert('Загаданное число меньше. Пробуйте еще.');
+		}
+	else {
+		if (num < r) {
+		alert('Загаданное число больше. Пробуйте еще.');
+	}
+	}
+	}
+	}
+}
